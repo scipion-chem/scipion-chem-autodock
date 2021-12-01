df93f22dde1e7d1776927a86058e4baf1fbc7000
DaniDelHoyo <danieldelhoyog@gmail.com>
2021-12-01 12:41:52 +0100

    Pocket viewers from ViewerGeneralPockets

--- autodock/viewers/viewer_pockets.py
+++ autodock/viewers/viewer_pockets.py
@@ -23,72 +23,13 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
-import os
-
 from ..protocols import ProtChemAutoLigand
-import pyworkflow.protocol.params as params
-import pyworkflow.viewer as pwviewer
-from pwchem.viewers import PyMolViewer, PocketPointsViewer, ContactSurfaceViewer
+from pwchem.viewers import ViewerGeneralPockets
 
 
-VOLUME_PYMOL, VOLUME_PYMOL_SURF = 0, 1
-
-class viewerAutoLigand(pwviewer.ProtocolViewer):
-  _label = 'Viewer pockets'
+class viewerAutoLigand(ViewerGeneralPockets):
+  _label = 'Viewer pockets AutoLigand'
   _targets = [ProtChemAutoLigand]
 
   def __init__(self, **kwargs):
-    pwviewer.ProtocolViewer.__init__(self, **kwargs)
-
-  def _defineParams(self, form):
-    form.addSection(label='Visualization of predicted pockets')
-    form.addParam('displayAtomStruct', params.EnumParam,
-                  choices=['PyMol (Pocket Points)', 'PyMol (Contact Surface)'],
-                  default=VOLUME_PYMOL,
-                  display=params.EnumParam.DISPLAY_HLIST,
-                  label='Display output AtomStruct with',
-                  help='*PyMol*: display AtomStruct and pockets as points / surface.')
-    form.addParam('displayBBoxes', params.BooleanParam,
-                  default=False, label='Display pocket bounding boxes',
-                  help='Display the bounding boxes in pymol to check the size for the localized docking')
-    form.addParam('pocketRadiusN', params.FloatParam, label='Grid radius vs pocket radius: ',
-                  default=1.1, condition='displayBBoxes',
-                  help='The radius * n of each pocket will be used as grid radius')
-
-  def _getVisualizeDict(self):
-    return {
-      'displayAtomStruct': self._showAtomStruct,
-    }
-
-  def _validate(self):
-    return []
-
-  # =========================================================================
-  # ShowAtomStructs
-  # =========================================================================
-
-  def getOutputAtomStructFile(self):
-    return os.path.abspath(self.protocol.outputAtomStruct.getFileName())
-
-  def _showAtomStruct(self, paramName=None):
-    if self.displayAtomStruct == VOLUME_PYMOL:
-      return self._showAtomStructPyMol()
-
-    elif self.displayAtomStruct == VOLUME_PYMOL_SURF:
-      return self._showAtomStructPyMolSurf()
-
-  def _showAtomStructPyMol(self):
-    bBox = self.displayBBoxes.get()
-    if bBox:
-      bBox = self.pocketRadiusN.get()
-
-    pymolV = PocketPointsViewer(project=self.getProject())
-    pymolV._visualize(self.protocol.outputPockets, bBox=bBox)
-
-  def _showAtomStructPyMolSurf(self):
-    bBox = self.displayBBoxes.get()
-    if bBox:
-      bBox = self.pocketRadiusN.get()
-
-    pymolV = ContactSurfaceViewer(project=self.getProject())
-    pymolV._visualize(self.protocol.outputPockets, bBox=bBox)
+    super().__init__(**kwargs)
