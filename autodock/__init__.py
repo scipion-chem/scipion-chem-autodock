008ec78a882ea75534600a29874d6c2bdbdf1e70
MartinSalinas98 <ssalinasmartin@gmail.com>
2023-06-01 17:50:18 +0200

    Typos

--- autodock/__init__.py
+++ autodock/__init__.py
@@ -4,7 +4,7 @@
 # *			 	Daniel Del Hoyo Gomez (ddelhoyo@cnb.csic.es)
 # *			 	Martín Salinas Antón (martin.salinas@cnb.csic.es)
 # *
-# * Unidad deBioinformatica of Centro Nacional de Biotecnologia , CSIC
+# * Unidad de Bioinformatica of Centro Nacional de Biotecnologia, CSIC
 # *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
@@ -13,16 +13,16 @@
 # *
 # * This program is distributed in the hope that it will be useful,
 # * but WITHOUT ANY WARRANTY; without even the implied warranty of
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # * GNU General Public License for more details.
 # *
 # * You should have received a copy of the GNU General Public License
 # * along with this program; if not, write to the Free Software
 # * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-# * 02111-1307USA
+# * 02111-1307  USA
 # *
-# *All comments concerning this program package may be sent to the
-# *e-mail address 'scipion@cnb.csic.es'
+# * All comments concerning this program package may be sent to the
+# * e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
 """
