1bf4afd50d803fcddf49ce3733c6ccab0fefb0c9
DaniDelHoyo <danieldelhoyog@gmail.com>
2025-09-22 15:22:22 +0200

    -y in conda install

--- autodock/__init__.py
+++ autodock/__init__.py
@@ -238,7 +238,7 @@ class Plugin(pwchemPlugin):
 												 'RINGTAIL_INSTALLED'). \
 			addCommand(f'{cls.getEnvActivationCommand(RDKIT_DIC)} && pip install --no-deps '
 								 f'{MEEKO_DIC["name"]}=={MEEKO_DIC["version"]} prody==2.4', 'MEEKO_INSTALLED'). \
-			addCommand(f'{cls.getEnvActivationCommand(RDKIT_DIC)} && conda install gemmi==0.7.3', 'MEEKO_DEPS_INSTALLED'). \
+			addCommand(f'{cls.getEnvActivationCommand(RDKIT_DIC)} && conda install -y gemmi==0.7.3', 'MEEKO_DEPS_INSTALLED'). \
 			addPackage(env, dependencies=['conda'], default=default)
 
 	@classmethod
