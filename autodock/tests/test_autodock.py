42ffa83ef4b77ff746481422aea022291905a7a2
Blanca Pueche <blancapueche@gmail.com>
2025-12-15 14:53:04 +0100

    changes so test does not fail

--- autodock/tests/test_autodock.py
+++ autodock/tests/test_autodock.py
@@ -249,7 +249,7 @@ class TestAutoDock(TestAutoSite, TestADMeekoLigands):
         protAutoLig = self._runAutoSiteFind()
         self._waitOutput(protAutoLig, 'outputStructROIs', sleepTime=5)
         self._runSetFilter(inProt=protAutoLig, number=2, property='_score')
-        self._waitOutput(self.protFilter, 'outputStructROIs', sleepTime=5)
+        self._waitOutput(self.protFilter, 'outputStructROIs', sleepTime=15)
 
         protAutoDock2 = self._runAutoDock(self.protFilter)
 
@@ -351,7 +351,7 @@ class TestAutoSitePharmacophore(TestAutoSite):
     def test(self):
         self._runPrepareReceptorADT()
         protAutoSite = self._runAutoSiteFind()
-        self._waitOutput(protAutoSite, 'outputStructROIs', sleepTime=5)
+        self._waitOutput(protAutoSite, 'outputStructROIs', sleepTime=15)
         protPharm = self._runAutoSitePharm(protAutoSite)
         self._waitOutput(protPharm, 'outputPharmacophore', sleepTime=5)
         assertHandle(self.assertIsNotNone, getattr(protPharm, 'outputPharmacophore', None), cwd=protPharm.getWorkingDir())
