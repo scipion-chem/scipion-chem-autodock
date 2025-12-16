4d13eb5ba644b6062d5178566347714e67256423
Blanca Pueche <blancapueche@gmail.com>
2025-12-16 09:16:29 +0100

    wait output in tests

--- autodock/tests/test_autodock.py
+++ autodock/tests/test_autodock.py
@@ -217,7 +217,7 @@ class TestAutoDock(TestAutoSite, TestADMeekoLigands):
         cls.protFilter.inputSet.set(inProt)
         cls.protFilter.inputSet.setExtended('outputStructROIs')
 
-        cls.proj.launchProtocol(cls.protFilter, wait=False)
+        cls.proj.launchProtocol(cls.protFilter, wait=True)
         return cls.protFilter
 
     def _runAutoDock(self, pocketsProt=None):
@@ -249,7 +249,7 @@ class TestAutoDock(TestAutoSite, TestADMeekoLigands):
         protAutoLig = self._runAutoSiteFind()
         self._waitOutput(protAutoLig, 'outputStructROIs', sleepTime=5)
         self._runSetFilter(inProt=protAutoLig, number=2, property='_score')
-        self._waitOutput(self.protFilter, 'outputStructROIs', sleepTime=30)
+        self._waitOutput(self.protFilter, 'outputStructROIs', sleepTime=10)
 
         protAutoDock2 = self._runAutoDock(self.protFilter)
 
@@ -345,13 +345,13 @@ class TestAutoSitePharmacophore(TestAutoSite):
             inputStructROIs=protAutoSite.outputStructROIs,
             inputStructROISelect='Structural ROI 1, AutoSite class')
 
-        cls.proj.launchProtocol(protPocketFinder, wait=False)
+        cls.proj.launchProtocol(protPocketFinder, wait=True)
         return protPocketFinder
 
     def test(self):
         self._runPrepareReceptorADT()
         protAutoSite = self._runAutoSiteFind()
-        self._waitOutput(protAutoSite, 'outputStructROIs', sleepTime=30)
+        self._waitOutput(protAutoSite, 'outputStructROIs', sleepTime=10)
         protPharm = self._runAutoSitePharm(protAutoSite)
         self._waitOutput(protPharm, 'outputPharmacophore', sleepTime=5)
         assertHandle(self.assertIsNotNone, getattr(protPharm, 'outputPharmacophore', None), cwd=protPharm.getWorkingDir())
