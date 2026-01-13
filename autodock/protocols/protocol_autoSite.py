6ef967bdf06d29fb38a74baeaaf312e5c2b7a32c
Blanca Pueche <blancapueche@gmail.com>
2026-01-13 10:34:59 +0100

    suggested changes

--- autodock/protocols/protocol_autoSite.py
+++ autodock/protocols/protocol_autoSite.py
@@ -114,10 +114,9 @@ class ProtChemAutoSite(ProtChemAutodockBase):
             pock.setVolume(pock.getPocketVolume())
             outPockets.append(pock)
 
-        self._defineOutputs(outputStructROIs=outPockets)
-
         if len(outPockets) > 0:
             outPockets.buildPDBhetatmFile()
+            self._defineOutputs(outputStructROIs=outPockets)
 
 
     # --------------------------- Utils functions --------------------
