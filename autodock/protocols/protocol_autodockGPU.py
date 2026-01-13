a14759a67d8262a56914d6f34d489da90a711894
Blanca Pueche <blancapueche@gmail.com>
2026-01-13 09:31:48 +0100

    suggested changes

--- autodock/protocols/protocol_autodockGPU.py
+++ autodock/protocols/protocol_autodockGPU.py
@@ -24,7 +24,7 @@
 # *
 # **************************************************************************
 
-import os, glob
+import os, glob, re
 
 from pyworkflow.protocol.params import IntParam, FloatParam, BooleanParam, \
   LEVEL_ADVANCED, USE_GPU, GPU_LIST, StringParam, EnumParam
@@ -236,10 +236,8 @@ class ProtChemAutodockGPU(ProtChemAutodockBase):
     for smallMol in mols:
       molFile = smallMol.getFileName()
       molName = getBaseName(molFile)
-      matchingKeys = [
-          k for k in pocketDic
-          if k == molName or k.startswith(molName + '-') or k.startswith('g') and molName in k
-      ]
+      pattern = re.compile(rf'^(?:{re.escape(molName)}(?:-|$)|g_.*\b{re.escape(molName)}\b)')
+      matchingKeys = [k for k in pocketDic if pattern.search(k)]
       if not matchingKeys:
           continue
 
