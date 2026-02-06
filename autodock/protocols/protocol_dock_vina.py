8fdcc0b9b979fa9a72644e232839bbc711079e2d
DaniDelHoyo <danieldelhoyog@gmail.com>
2026-02-06 11:23:30 +0100

    MolNames without confId

--- autodock/protocols/protocol_dock_vina.py
+++ autodock/protocols/protocol_dock_vina.py
@@ -213,7 +213,7 @@ search and scoring behaviors."""
               pocketDic[molName] = self.parseDockedPDBQT(dockFile)
 
           for smallMol in self.inputSmallMolecules.get():
-              molName = smallMol.getUniqueName(conf=True)
+              molName = smallMol.getUniqueName(grid=False, conf=False, pose=False, dock=False)
               if molName in pocketDic:
                 molDic = pocketDic[molName]
 
