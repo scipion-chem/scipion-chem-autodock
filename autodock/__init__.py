9dfa77ce09f0ac5c9caedd74ba74b21d179020f4
judith-MC <judith3atk@gmail.com>
2026-06-29 10:24:56 +0200

    Refactor environment variable and make command variables
    Sonar recommendations

--- autodock/__init__.py
+++ autodock/__init__.py
@@ -152,13 +152,13 @@ class Plugin(pwchemPlugin):
             targetsFlag = f' TARGETS={compCap}' if compCap else ''
 
             # Installing package
-            env_str = ' '.join(f'{k}={v}' for k, v in enVars.items())
-            make_cmd = f'cd {cls._atdgpuBinary} && {env_str} CC=/usr/bin/gcc-12 CXX=/usr/bin/g++-12 make DEVICE=GPU OVERLAP=ON{targetsFlag}'
+            envStr = ' '.join(f'{k}={v}' for k, v in enVars.items())
+            makeCmd = f'cd {cls._atdgpuBinary} && {envStr} CC=/usr/bin/gcc-12 CXX=/usr/bin/g++-12 make DEVICE=GPU OVERLAP=ON{targetsFlag}'
 
             installer.getCloneCommand(cls.getAutoDockGPUGithub(),
                                       binaryFolderName=cls._atdgpuBinary,
                                       targeName='ATDGPU_CLONED') \
-                .addCommand(make_cmd, 'ATDGPU_COMPILED') \
+                .addCommand(makeCmd, 'ATDGPU_COMPILED') \
                 .addPackage(env, dependencies=['git', 'make'], default=default)
 
     @classmethod
